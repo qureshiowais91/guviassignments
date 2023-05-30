44a1c0cda922fa53ea27c280d8fff54500c3e1d6
qureshiowais <qureshiowais91@gmail.com>
2023-05-30 16:32:46 +0530

    init

--- main.js
+++
@@ -1,6 +0,0 @@
-let navBar = document.querySelector(".navbar");
-
-navBar.addEventListener("click", (evt) => {
-  document.querySelector(".active").classList.remove("active");
-  evt.target.classList.add("active");
-});
